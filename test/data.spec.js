1b4555a318cd192c7469c5210f0e79929cf6a527
ElizabethMaciasSandoval <elizabeth130394@gmail.com>
2021-10-20 09:15:11 -0300

    trabajamos en desarrollar los tests para las funciones que filtran

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,41 @@
+
 import { filterDataDirector, filterDataProducer } from '../src/data.js';
 
 
-describe('example', () => {
+describe('filterDataDirector', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterDataDirector).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('should return the result to director `Hayao Miyazaki`', () => {
+    const data = [
+      { director: 'Gorō Miyazaki' },
+      { director: 'Hayao Miyazaki' },
+      { director: 'Hiromasa Yonebayashi' },
+      { director: 'Hiroyuki Morita' },
+      { director: 'Isao Takahata' },
+      { director: 'Yoshifumi Kondō' }
+    ]
+    const result = [{ director: 'Hayao Miyazaki' }]
+    expect(filterDataDirector(data, 'Hayao Miyazaki')).toEqual(result);
   });
 });
 
-
-describe('anotherExample', () => {
+describe('filterDataProducer', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filterDataProducer).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('should return the result to producer `Hayao Miyazaki`', () => {
+    const data = [
+      { producer: 'Hayao Miyazaki' },
+      { producer: 'Isao Takahata' },
+      { producer: 'Toru Hara' },
+      { producer: 'Toshio Suzuki' },
+      { producer: 'Yoshiaki Nishimura' },
+    ]
+    const result = [{ producer: 'Hayao Miyazaki' }]
+    expect(filterDataProducer(data, 'Hayao Miyazaki')).toEqual(result);
   });
 });
+
